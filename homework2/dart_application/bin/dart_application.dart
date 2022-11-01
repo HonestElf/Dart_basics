c05bcace212a94d56acebd639dca616b512c615b
Vasiliy <prokudinvasya@gmail.com>
2022-11-01 19:07:09 +0300

    start dart homework

---
+++ homework2/dart_application/bin/dart_application.dart
@@ -0,0 +1,8 @@
+// import 'package:dart_application/dart_application.dart' as dart_application;
+
+import 'package:dart_application/gcd.dart';
+
+void main(List<String> arguments) {
+  // print('Hello world: ${dart_application.calculate()}!');
+  print(calculateGcd(15, 10));
+}
---
+++ homework2/dart_application/lib/gcd.dart
@@ -0,0 +1,8 @@
+List<int> calculateGcd(int a, int b) {
+  List<int> list = [];
+
+  list.add(a);
+  list.add(b);
+
+  return list;
+}
